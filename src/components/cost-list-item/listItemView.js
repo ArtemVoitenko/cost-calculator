6dcbeece9ac2acc210763ee61e4673d9ac173ca7
unknown <artvoitenk072@gmail.com>
2019-04-10 23:45:56 +0300

    Add icon component

--- src/components/cost-list-item/listItemView.js
+++ src/components/cost-list-item/listItemView.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Icon from "../icon/icon";
 
 const ListItemView = ({
   actionDate,
@@ -12,27 +13,37 @@ const ListItemView = ({
   getItemInfo
 }) => {
   return (
-    <li id={actionId} className="cost-list__item">
+    <li
+      id={actionId}
+      className={`cost-list__item cost-list__item--${actionType}`}
+    >
       <div className={`purpose-image purpose-image--${actionPurpose}`} />
       <div className="cost-list__name">{actionName}</div>
-      <div className={`cost-list__sum cost-list__sum--${actionType}`}>
-        {actionSum}
-      </div>
+
       <div className="cost-list__date">{actionDate}</div>
+      <div className={`cost-list__sum cost-list__sum--${actionType}`}>
+        {actionSum}&#x20b4;
+      </div>
       <div className="cost-list__buttons">
-        <button className="cost-list__edit" onClick={onEditClick}>
-          edit
+        <button
+          className=" cost-list__btn cost-list__edit"
+          onClick={onEditClick}
+        >
+          <Icon icon="edit" iconClass="cost-list__icon" />
         </button>
         <button
-          className="cost-list__remove"
+          className=" cost-list__btn cost-list__remove"
           onClick={() => {
             onRemove(actionId);
           }}
         >
-          remove
+          <Icon icon="delete" iconClass="cost-list__icon" />
         </button>
-        <button className="cost-list__more-info" onClick={getItemInfo}>
-          info
+        <button
+          className=" cost-list__btn cost-list__more-info"
+          onClick={getItemInfo}
+        >
+          <Icon icon="information" iconClass="cost-list__icon" />
         </button>
       </div>
     </li>
